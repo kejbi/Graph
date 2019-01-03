17122ae6a6ddcc962fd2fae7247462ad46d36df8
kejbi <biegajski.kacper@gmail.com>
2019-01-03 23:54:22 +0100

    finding bridges, passing tests

--- Graph.h
+++ Graph.h
@@ -6,12 +6,16 @@
 #define GRAPHS_GRAPH_H
 
 #include <vector>
+#include <algorithm>
 
 class Graph{
 public:
     using adjVector = std::vector<std::vector<int>>;
+    using edge = std::pair<int,int>;
+    using edgesVector = std::vector<std::pair<int,int>>;
 private:
     adjVector adjacency;
+    edgesVector edges;
     int vertices;
 
     void dfs(int startVertex, adjVector& adjacency, bool visited[]){
@@ -24,19 +28,47 @@ private:
         }
     }
 
+    bool isExtensiveBridge(edge& edge) {
+        auto newAdjacency = adjacency;
+        int startVertex = -1;
 
+        for (auto i = 0; i < vertices; ++i) {
+            if (i != edge.first && i != edge.second) {
+                newAdjacency[i].erase(std::remove_if(newAdjacency[i].begin(), newAdjacency[i].end(), [&edge](int vertex) {
+                    return vertex == edge.first || vertex == edge.second;
+                }),newAdjacency[i].end());
+                startVertex = i;
+            }
+        }
 
+        if (startVertex == -1) return false;
 
+        bool visited[vertices] = {false};
+        dfs(startVertex, newAdjacency, visited);
+        auto verticesCounted = std::count_if(visited, visited + vertices, [](bool visit) { return visit; });
+
+        return verticesCounted + 2 != vertices;
+    }
 
 public:
-    Graph():adjacency(adjVector()), vertices(0){}
+    Graph():adjacency(adjVector()),edges(edgesVector()), vertices(0){}
 
-    Graph(int vertices, std::vector<std::pair<int,int>>& edges):vertices(vertices),
+    Graph(int vertices, edgesVector & edges):vertices(vertices), edges(edges),
                                                                adjacency(adjVector(vertices)){
         for(auto edge: edges){
             adjacency[edge.first].push_back(edge.second);
             adjacency[edge.second].push_back(edge.first);
         }
+    }
+
+    edgesVector getExtensiveBridges(){
+        edgesVector bridges;
+        for(auto& edge: edges){
+            if(isExtensiveBridge(edge)){
+                bridges.push_back(edge);
+            }
+        }
+        return bridges;
     }
 
     adjVector& getAdjacency(){
--- tests.cpp
+++ tests.cpp
@@ -19,12 +19,32 @@ TEST_CASE("Graph Constructor"){
     REQUIRE(adj[2][0] == 1);
     REQUIRE(adj[2][1] == 0);
 }
-TEST_CASE("DFS"){
-    std::vector<std::pair<int,int>> edges = {{1, 2}, {0, 2}};
-    Graph g(3, edges);
-    bool visited[3] = {false};
-    g.dfs(0,g.getAdjacency(),visited);
-    REQUIRE(visited[0] == true);
-    REQUIRE(visited[1] == true);
-    REQUIRE(visited[2] == true);
+//TEST_CASE("DFS"){
+//    std::vector<std::pair<int,int>> edges = {{1, 2}, {0, 2}};
+//    Graph g(3, edges);
+//    bool visited[3] = {false};
+//    g.dfs(0,g.getAdjacency(),visited);
+//    REQUIRE(visited[0] == true);
+//    REQUIRE(visited[1] == true);
+//    REQUIRE(visited[2] == true);
+//}
+TEST_CASE("Extensive Bridges, empty graph"){
+    std::vector<std::pair<int,int>> edges = {};
+    Graph g(0, edges);
+    auto bridges = g.getExtensiveBridges();
+    REQUIRE(bridges == edges);
+}
+TEST_CASE("Extensive Bridges, not empty graph"){
+    std::vector<std::pair<int,int>> edges = {{0,1},{0,2},{0,3},{1,2},{2,3}};
+    Graph g(4, edges);
+    auto foundBridges = g.getExtensiveBridges();
+    std::vector<std::pair<int,int>> requiredBridges = {{0,2}};
+    REQUIRE(foundBridges == requiredBridges);
+}
+TEST_CASE("Extensive Bridges, not empty graph, no bridges"){
+    std::vector<std::pair<int,int>> edges = {{0,1},{0,2},{0,3},{1,2},{2,3},{1,3}};
+    Graph g(4, edges);
+    auto foundBridges = g.getExtensiveBridges();
+    std::vector<std::pair<int,int>> requiredBridges = {};
+    REQUIRE(foundBridges == requiredBridges);
 }
